0eadc242659cb4eb0210befe5784be4c576f4e4c
favorov <favorov@8a3b1b92-0160-11de-9ed6-7f1a95ea1535>
2012-11-26 10:29:30 +0000

    reads, mutations, mutations.vcf
    git-svn-id: file:///home/favorov/svnroot/trunk/coverage@1823 8a3b1b92-0160-11de-9ed6-7f1a95ea1535

--- pipeline-random/pipeline.pl
+++ pipeline-random/pipeline.pl
@@ -39,7 +39,7 @@ my $cfile_name=$ARGV[0];
 
 open( CONFIG, $cfile_name ) or print "Can't open config file $cfile_name. Error is  '$!' \n" and exit;
 
-my ($fasta_file, $sample_id, $mutations_file);
+my ($fasta_file, $sample_id, $mutations_file, $reads_file);
 
 while (<CONFIG>) {
 	chomp;
@@ -50,17 +50,46 @@ while (<CONFIG>) {
 	$line[1] = "" if not defined $line[1];
 
 	#	print "\'$line[0]\' = \'$line[1]\'\n";
-	$fasta_file     				= $line[1] if $line[0] eq 'fasta_file';
-	$sample_id     				= $line[1] if $line[0] eq 'sample_id';
+	$fasta_file	= $line[1] if $line[0] eq 'fasta_file';
+	$sample_id 	= $line[1] if $line[0] eq 'sample_id';
+	$reads_file = $line[1] if $line[0] eq 'reads_file';
 	$mutations_file = $line[1] if $line[0] eq 'mutations_file';
 }
 
-print "#Random coverage mutatition test pipeline. Config file is $ARGV[0], sample id is $sample_id.";
+print "#Random coverage mutatition test pipeline. Config file is $ARGV[0], sample id is $sample_id.\n";
 
-if (! -e $mutations_file) 
+if (! -e $mutations_file || ! -e $reads_file) 
 {
 	my $cheapseq_string=$cheapseq_folder."/cheapseq";
 	print "#Start cheapseq...\n";
 	system($cheapseq_string,$cfile_name) == 0 or die ("Cheapseq start failed: $?\n");
 	print "#Finished (cheapseq) ...\n";
 }
+else
+{
+	print "#Mutations file and read files exist.\n";
+}
+
+my $vcf_mutations_file=$mutations_file.".vcf";
+
+if ( -e $vcf_mutations_file ) 
+{
+	my $vcf_write_secs = (stat($vcf_mutations_file))[9];
+	my $mut_write_secs = (stat($mutations_file))[9];
+	unlink $vcf_mutations_file if $vcf_write_secs<$mut_write_secs;
+	#vcf file is to be younger than mutations file
+}
+
+if ( ! -e $vcf_mutations_file ) 
+{
+	#my $report2vcf_string="perl -I $report2vcf_folder $report2vcf_folder."/report2vcf.pl";
+	my $report2vcf_string="perl -I $report2vcf_folder $report2vcf_folder/report2vcf.pl $cfile_name > $vcf_mutations_file";
+	print "#Start report->vcf...\n";
+	system($report2vcf_string) == 0 or die ("Report2vcf.pl start failed: $?\n");
+	print "#Finished (report->vcf) ...\n";
+}
+else
+{
+	print "#Mutations file is already converted to vcf.\n";
+}
+
