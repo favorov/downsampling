e3f4c91e050150271f5f4f6fdd8aabfcdec1db61
favorov <favorov@8a3b1b92-0160-11de-9ed6-7f1a95ea1535>
2012-12-13 02:14:44 +0000

    propset
    git-svn-id: file:///home/favorov/svnroot/trunk/coverage@1849 8a3b1b92-0160-11de-9ed6-7f1a95ea1535

--- pipeline-random/cure_all_bams.py
+++ pipeline-random/cure_all_bams.py
@@ -1,4 +1,4 @@
-#$Id: $
+#$Id$
 
 import glob
 import os.path
--- pipeline-random/plot_pipeline_report.R
+++ pipeline-random/plot_pipeline_report.R
@@ -2,7 +2,9 @@ opts <- commandArgs(trailingOnly = TRUE)
 pipeline.results<-read.table(opts[1],header=TRUE)
 sample_name<-strsplit(opts[1],'\\.')[[1]][[1]]
 plotname<-paste(sample_name,".png",sep="")
-TPR=pipeline.results[,'intersection']/(pipeline.results[,'intersection']+pipeline.results[,'original_only'])
+TPR=pipeline.results[,'intersection']/
+#test line
+print (pipeline.results[,'intersection']+pipeline.results[,'original_only'])
 png(plotname)
 plot(pipeline.results[,'e_cov'],TPR,main=sample_name,ylim=c(0,1),xlab='coverage')
 dev.off()
